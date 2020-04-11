1cb9ac319fe8f2a2b1340a4a0eaedc21c4aec91e
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-11 20:04:14 +1000

    Add files via upload

---
+++ mywork/Nestes_Repetition/Exercise1/ex1.1/axl103083020.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name. ");
+            string Name = Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+
+                
+
+            Console.WriteLine(Name + " ");
+            }
+                
+
+        }
+    }
+}
