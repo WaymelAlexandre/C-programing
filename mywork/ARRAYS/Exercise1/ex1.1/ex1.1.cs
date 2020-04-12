aab45e792e97c8c2cc5a39d743610ffbeca26358
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:13:27 +1000

    Add files via upload

---
+++ mywork/ARRAYS/Exercise1/ex1.1/ex1.1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("how many are you ?? ");
+            
+            string inputString = Console.ReadLine();
+            int input = int.Parse(inputString);
+
+            string [] noms = new string[input];
+
+            for (int i = 0; i < noms.Length; i++)
+            {
+                Console.WriteLine(" Names pls ");
+                
+                noms[i] = Console.ReadLine();
+
+                
+            }
+            for (int i = 0; i < noms.Length; i++)
+            {
+                Console.WriteLine("bonjours " + noms[i] + " !!");
+            }
+
+            
+            
+    
+
+            
+
+        }
+    }
+}
