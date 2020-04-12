57dc26b6393fee1417549459107af5f46d9be6a1
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:10:27 +1000

    Add files via upload

---
+++ mywork/Nestes_Repetition/exercise2/ex3/AXL103083020.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int col = 0; col<5; col++)
+            {
+                
+
+                for (int rang = 0; rang < col; rang++)
+                {
+                  Console.Write("X");  
+                }
+            Console.WriteLine("X");   
+            }
+            
+        }
+    }
+}
