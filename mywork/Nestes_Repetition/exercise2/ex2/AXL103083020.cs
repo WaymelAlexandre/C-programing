278b31b8a9b57b8f032b9230df121a8947fdb792
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:08:53 +1000

    Add files via upload

---
+++ mywork/Nestes_Repetition/exercise2/ex2/AXL103083020.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("how whyle you want to be ?? ");
+            
+            string inputString = Console.ReadLine();
+            int input = int.Parse(inputString);
+
+            for (int rang = input; rang > 0; rang--)
+            {
+                
+                
+                for (int col = 1; col < rang; col++)
+                {
+                    Console.Write("X");            
+                }
+              
+            Console.WriteLine("X");
+              
+            }
+        }
+    }
+}
