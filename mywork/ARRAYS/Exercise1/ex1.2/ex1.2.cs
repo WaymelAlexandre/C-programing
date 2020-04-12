0de84ef10531d0efded6be21d2aa4665b19f1e5d
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:14:15 +1000

    Add files via upload

---
+++ mywork/ARRAYS/Exercise1/ex1.2/ex1.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ex1._2
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int [] num = {34, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+            int n1 = num.Length-1;
+            
+            
+            
+
+            for (int i = 0; i < num.Length; i++) 
+            {
+                
+               Console.Write(num[i]);
+               if (i<n1)
+               {
+                   Console.Write(" +  ");
+               }
+
+            
+            }           
+            Console.Write(" = "); 
+            Console.WriteLine(num.Sum());
+
+
+            
+                
+        }
+    }
+}
