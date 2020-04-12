e8f810d7e391637ea906aa3871b278be935aaef1
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:10:08 +1000

    Add files via upload

---
+++ mywork/Nestes_Repetition/exercise2/ex4/AXL103083020.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Size of the triangle");
+
+        string inputString = Console.ReadLine();
+            int input = int.Parse(inputString);
+
+            for (int col = 0; col<input; col++)
+            {
+                
+
+                for (int rang = 0; rang < col; rang++)
+                {
+                  Console.Write("X");  
+                }
+            Console.WriteLine("X");   
+            }
+            
+        }
+               
+           
+    }
+}
