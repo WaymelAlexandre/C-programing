91d67e24a3c1329c519d9108a3f0404ff64d9404
WaymelAlexandre <62829131+WaymelAlexandre@users.noreply.github.com>
2020-04-12 13:17:12 +1000

    Add files via upload

---
+++ mywork/ARRAYS/Exercise2/ex1.3/ex1.3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace ex1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<string> Names = new List<string>();    
+            
+
+            while (true)
+            {
+            Names.Add(Console.ReadLine());
+
+                if (Names.Contains("stop"))
+                {
+                    Names.Remove("stop"); 
+                    Names.Reverse();
+
+                    foreach (string item in Names)
+                    {
+                        Console.WriteLine(item);                    
+                    }
+                break;
+                }
+                
+            }
+            
+            
+        }
+    }
+}
